d0ba4406e29cfeb00021c273880bb3b7ce4dcc25
queirozws <queirozws@gmail.com>
2021-08-09 14:47:52 -0300

    sss

--- geeScripts/script.js
+++ geeScripts/script.js
@@ -2,10 +2,12 @@ const ee = require('@google/earthengine');
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-const print = require('/home/wsq/mapbiomas/get-thumbnails-from-gee/auxiliar/print.js'); // Por que funciona apenas com full path?
+const print = require('../print.js'); // Por que funciona apenas com full path?
 
 async function download(url, filename, format) {
     const response = await fetch(url);
+
+    // print
     
     const buffer = await response.buffer();
 
